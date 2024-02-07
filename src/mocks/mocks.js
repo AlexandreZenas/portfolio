5959922d799d2caf81d746fb6fa3c3ddc4d49f34
alexandrezenas <94380403+AlexandreZenas@users.noreply.github.com>
2024-02-06 22:27:54 -0300

    deploy

--- src/mocks/mocks.js
+++ src/mocks/mocks.js
@@ -7,48 +7,48 @@ export const dados = [
     },
     {
         link: 'https://alexandrezenas.github.io/impulseon/',
-        image:'/images/animation-3d-print.png',
+        image:'images/animation-3d-print.png',
         title: 'Animação 3d',
         text: 'Interação ao passar o cursor do mouse em cima dos cards '
     },
     {
         link: 'https://alexandrezenas.github.io/RJTour/',
-        image:'public/images/rjtour-print.png',
+        image:'images/rjtour-print.png',
         title: 'Scroll tiktok',
         text: 'Ao scrollar o mouse, as páginas ganham um efeito de folheamento.'
     },
     {
         link: 'https://alexandrezenas.github.io/UpGrade/',
-        image:'/public/images/upgrade-print.png',
+        image:'images/upgrade-print.png',
         title: 'Video em loop',
         text: 'Manipulação de 2 videos, uma intro e outro em loop'
     },
     {
         link: '/',
-        image:'./public/images/backgroundBanner.jpg',
+        image:'images/backgroundBanner.jpg',
         title: 'Em desenvolvimento',
         text: ':/'
     },
     {
         link: '/',
-        image:'/images/backgroundBanner.jpg',
+        image:'images/backgroundBanner.jpg',
         title: 'Em desenvolvimento',
         text: ':/'
     }
 ]
 export const contentMainContainer = [
     {
-        image:'/images/multiScreens.png',
+        image:'images/multiScreens.png',
         title: 'Projetos',
         text: ' Confira alguns dos meus projetos e cases de design e desenvolvimento de páginas.'
     },
     {
-        image:'/images/photoBanner.png',
+        image:'images/photoBanner.png',
         title: 'Sobre',
         text: ' Ainda em desenvolvimento :/'
     },
     {
-        image:'/images/photoBanner.png',
+        image:'images/photoBanner.png',
         title: 'Contato',
         text: 'Me mande uma mensagem por email ou entre em contato nas redes sociais.'
     }
